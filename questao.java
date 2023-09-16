ac1dfa059e02bb03ee5135b14aac1a7ee6a959bf
Heloisa Mendes <108590744+heloomendess@users.noreply.github.com>
2023-09-15 21:41:48 -0300

    questão de multipla escolha com switch

---
+++ questao.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class questao {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Qual é o resultado da raiz quadrada de 4?:");
+        System.out.println("a) 5");
+        System.out.println("b) 4");
+        System.out.println("c) 3");
+        System.out.println("d) 2");
+
+        System.out.println("Digite sua resposta: ");
+        int resposta = entrada.nextInt();
+
+        switch (resposta) {
+            case 2:
+                System.out.println("Resposta correta!");
+            break;
+            default:
+                System.out.println("Resposta incorreta.");
+        }
+    }
+}
