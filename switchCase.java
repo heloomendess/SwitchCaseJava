0f8db1cd6f85a9271977b53fb58a3cfa02d03d35
Heloisa Mendes <108590744+heloomendess@users.noreply.github.com>
2023-09-15 21:31:29 -0300

    switch case PI (dias da semana)

--- switchCase.java
+++ switchCase.java
@@ -1,46 +1,38 @@
 import java.util.Scanner;
+
 public class switchCase {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Digite a primeira nota: ");
-        int n1 = input.nextInt();
+        System.out.print("Escreva um valor de 1 a 7 que corresponde a um dia da semana: ");
+        int valor = entrada.nextInt();
 
-        System.out.print("Digite a segunda nota: ");
-        int n2 = input.nextInt();
-
-        int media = (n1 + n2)/2;
-
-        if (media > 9) {
-            media = 'A';
-        } else if (media >= 8) {
-            media = 'B';
-        } else if (media >= 7) {
-            media = 'C';
-        } else if (media >= 6) {
-            media = 'D';
-        } else {
-            media = 'E';
-        }
-        switch (media) {
-           case 'A':
-                    System.out.println("Sua nota foi A, excelente!");
-                break;
-           case 'B':
-                    System.out.println("Sua nota foi B, excelente!");
-                break;
-            case 'C':
-                    System.out.println("Sua nota foi C, bem feito!");
-                break;
-            case 'D':
-                    System.out.println("Sua nota foi D, você passou!");
-                break;
-            case 'E':
-                    System.out.println("Sua nota foi E, melhor tentar novamente!");
-                break;
+        switch (valor) {
+            case 1:
+                System.out.println("Hoje é: Domingo.");
+            break;
+            case 2:
+                System.out.println("Hoje é: Segunda-feira.");
+            break;
+            case 3:
+                System.out.println("Hoje é: Terça-feira.");
+            break;
+            case 4:
+                System.out.println("Hoje é: Quarta-feira.");
+            break;
+            case 5:
+                System.out.println("Hoje é: Quinta-feira");
+            break;
+            case 6:
+                System.out.println("Hoje é: Sexta-feira.");
+            break;
+            case 7:
+                System.out.println("Hoje é: Sábado.");
+            break;
             default:
-                    System.out.println("Conceito inválido.");       
+                System.out.println("Valor inválido.");
+
         }
-        input.close();
+        entrada.close();
     }
 }
